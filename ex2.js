57cee5bae4c8f5dd84ea684e237cd6640b165e6f
Vinicius Espindula <dev.viniciusesp@gmail.com>
2023-07-05 23:56:03 -0300

    first commit

---
+++ ex2.js
@@ -0,0 +1,8 @@
+
+var numeros = [13, 25, 270, 5, 70];
+var soma = 0;
+
+for (var numero of numeros) {
+  soma += numero;
+}
+console.log("A soma dos números é:", soma);
---
+++ ex3.js
@@ -0,0 +1,9 @@
+
+var frutas = ["maçã", "banana", "laranja"];
+
+frutas.push("abacaxi");
+frutas.pop();
+frutas.unshift("uva");
+frutas.shift();
+
+console.log(frutas);
